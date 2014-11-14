1e1b5dbc8994a782ce6ae96373ff8c77907211fc
CJKu <cjcool.tw@gmail.com>
2014-11-14 14:20:22 +0800

    draft version

--- adbwifi.sh
+++ adbwifi.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-$(adb shell ip -f inet addr show wlan0 2>/dev/null)
+_get_ip ()
+{
+  local output="$(adb shell ip -f inet addr show wlan0 2>/dev/null | grep inet)"
+  local ip="$(echo ${output-""} | sed \
+    "s/[[:blank:]]*inet[[:blank:]]*\([[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*\).*/\1/")"
+
+  echo $ip
+}
+
+ip_address=$(_get_ip)
+adb tcpip 5555
+adb connect $ip_address
