02880baf602f6dcf6f847c5f8e870c95e775cff8
CJKu <cjcool.tw@gmail.com>
2014-11-14 14:51:26 +0800

    update

--- adbwifi.sh
+++ adbwifi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-_get_ip ()
+_get_device_addr ()
 {
   local output="$(adb shell ip -f inet addr show wlan0 2>/dev/null | grep inet)"
   local ip="$(echo ${output-""} | sed \
@@ -9,6 +9,9 @@ _get_ip ()
   echo $ip
 }
 
-ip_address=$(_get_ip)
-adb tcpip 5555
-adb connect $ip_address
+ip_address=$(_get_device_addr)
+if [ -n "$ip_address" ]
+then
+  adb tcpip 5555
+  adb connect $ip_address
+fi
